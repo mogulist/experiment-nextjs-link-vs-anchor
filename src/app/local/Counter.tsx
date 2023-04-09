a1d7402c7932bddb2d7e34c0476d88a025d20783
Herbert Lim <mogulist@naver.com>
2023-04-09 16:54:12 +0900

    Add Counter to local page

---
+++ src/app/local/Counter.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div className="flex  justify-between space-x-6">
+        <button className={buttonStyle} onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        <p className="text-xl">You clicked {count} times</p>
+        <button className={buttonStyle} onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const buttonStyle = "px-3 py-1 border-2 border-gray-300 rounded-lg";
--- src/app/local/page.tsx
+++ src/app/local/page.tsx
@@ -1,9 +1,11 @@
 import OutSideLinks from "@/components/OutSideLinks";
+import Counter from "@/app/local/Counter";
 
 const LocalStateUser = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl">Local State User</h1>
+      <Counter />
       <OutSideLinks />
     </main>
   );
